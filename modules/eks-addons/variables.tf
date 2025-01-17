85959b6ebd15ebce10192fe7c74e8271cde1f251
Laura Roque <lroquecu@gmail.com>
2025-01-17 15:15:19 +0100

    variables for eks-addons module

---
+++ modules/eks-addons/variables.tf
@@ -0,0 +1,49 @@
+variable "cluster_name" {
+  description = "Name of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_endpoint" {
+  description = "Endpoint of the EKS cluster"
+  type        = string
+}
+
+variable "cluster_certificate_authority_data" {
+  description = "Certificate authority data for the EKS cluster"
+  type        = string
+}
+
+variable "oidc_provider_arn" {
+  description = "ARN of the OIDC Provider"
+  type        = string
+}
+
+variable "enable_metrics_server" {
+  description = "Enable metrics server"
+  type        = bool
+  default     = true
+}
+
+variable "enable_cluster_autoscaler" {
+  description = "Enable cluster autoscaler"
+  type        = bool
+  default     = true
+}
+
+variable "enable_load_balancer_controller" {
+  description = "Enable AWS Load Balancer Controller"
+  type        = bool
+  default     = true
+}
+
+variable "enable_external_dns" {
+  description = "Enable external DNS"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
