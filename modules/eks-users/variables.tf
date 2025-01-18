5860742b986e5c31bc6ba954560b45d034cb36ba
Laura Roque <lroquecu@gmail.com>
2025-01-18 13:13:03 +0100

    add variables for user creation

--- modules/eks-users/variables.tf
+++ modules/eks-users/variables.tf
@@ -8,6 +8,54 @@ variable "environment" {
   type        = string
 }
 
+variable "admin_role" {
+  description = "Name of the admin role to create"
+  type        = string
+  default     = "eks-admin"
+}
+
+variable "developer_role" {
+  description = "Name of the developer role to create"
+  type        = string
+  default     = "eks-developer"
+}
+
+variable "readonly_role" {
+  description = "Name of the readonly role to create"
+  type        = string
+  default     = "eks-viewer"
+}
+
+variable "tags" {
+  description = "A map of tags to add to all resources"
+  type        = map(string)
+  default     = {}
+}
+
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "create_admin_users" {
+  description = "Whether to create admin IAM users"
+  type        = bool
+  default     = false
+}
+
+variable "create_developer_users" {
+  description = "Whether to create developer IAM users"
+  type        = bool
+  default     = false
+}
+
+variable "create_readonly_users" {
+  description = "Whether to create readonly IAM users"
+  type        = bool
+  default     = false
+}
+
 variable "admin_users" {
   description = "List of admin users to create"
   type        = list(string)
@@ -24,10 +72,4 @@ variable "readonly_users" {
   description = "List of readonly users to create"
   type        = list(string)
   default     = []
-}
-
-variable "tags" {
-  description = "A map of tags to add to all resources"
-  type        = map(string)
-  default     = {}
 }
